503793c5750b4e04bc362bdea14f43087ff1cf09
nihalanand <nihalanand@gmail.com>
2021-02-24 23:46:15 -0500

    completed checkUnit function and added windIcon

--- script.js
+++ script.js
@@ -29,25 +29,25 @@ weatherApp.getLocation = () => {
 
 weatherApp.checkUnit = () => {
     let checked = document.querySelector('input:checked');
-    console.log(checked.value)
-    
+    weatherApp.unit = checked.value;
+
     const selector = document.querySelector('form');
     selector.addEventListener('change', () => {
         checked = document.querySelector('input:checked');
-        console.log(checked.value)
+        weatherApp.unit = checked.value;
+        weatherApp.getLocation();
     })
-    
-    // let unit = checked.value;
-}
 
-weatherApp.checkUnit();
+
+}
 
 weatherApp.getWeather = userCoordinates => {
     const url = new URL(weatherApp.weatherUrl);
     url.search = new URLSearchParams({
         lat: userCoordinates.latitude,
         lon: userCoordinates.longitude,
-        appid: weatherApp.apiKey
+        appid: weatherApp.apiKey,
+        units: weatherApp.unit
     });
 
     fetch(url)
@@ -59,25 +59,31 @@ weatherApp.getWeather = userCoordinates => {
 };
 
 weatherApp.displayWeather = weatherObject => {
+    const allIcons = document.querySelectorAll('i:not(h1 i, form i)');
+    allIcons.forEach(icon => icon.remove());
+
     document.querySelector('.weather-title')
         .textContent = `Current weather in ${weatherObject.name}`;
 
     document.getElementById('real-temp')
-        .textContent = `${(weatherObject.main.temp - 273.15).toFixed()}°C`;
+        .textContent = `${(weatherObject.main.temp).toFixed()}°`;
 
     document.getElementById('feels-like')
-        .textContent = `${(weatherObject.main.feels_like - 273.15).toFixed()}°C`;
+        .textContent = `${(weatherObject.main.feels_like).toFixed()}°`;
 
     document.getElementById('description')
         .textContent = `${weatherObject.weather[0].description}`;
 
     const weatherIcon = document.createElement('i');
-    weatherIcon.classList.add('wi',`wi-owm-${weatherObject.weather[0].id}`);
+    weatherIcon.classList.add('wi', `wi-owm-${weatherObject.weather[0].id}`);
     weatherIcon.alt = `${weatherObject.weather[0].description}`;
     document.querySelector('.description').appendChild(weatherIcon);
 
+    const windIcon = document.createElement('i');
+    windIcon.classList.add('wi', 'wi-wind', `towards-${weatherObject.wind.deg}-deg`);
     document.getElementById('wind-speed')
-        .textContent = `${(weatherObject.wind.speed * 3.6).toFixed(1)} km/hr`
+        .textContent = ` ${(weatherObject.wind.speed * 3.6).toFixed(1)} ${weatherApp.unit === 'metric' ? 'km/hr' : 'mph'}`
+    document.getElementById('wind-speed').prepend(windIcon);
 
     document.getElementById('humidity')
         .textContent = `${weatherObject.main.humidity}%`
@@ -89,7 +95,8 @@ weatherApp.getForecast = userCoordinates => {
     url.search = new URLSearchParams({
         lat: userCoordinates.latitude,
         lon: userCoordinates.longitude,
-        appid: weatherApp.apiKey
+        appid: weatherApp.apiKey,
+        units: weatherApp.unit
     });
 
     fetch(url)
@@ -100,7 +107,7 @@ weatherApp.getForecast = userCoordinates => {
 weatherApp.showForecast = forecastData => {
     const currentDate = new Date(forecastData.current.dt * 1000);
     console.log(forecastData)
-    
+
     const currentDateElement = document.querySelector('#current-date');
     currentDateElement.textContent = `${currentDate.toLocaleString('en-US', { weekday: 'short', month: 'short', day: '2-digit'})}`;
 
@@ -108,28 +115,33 @@ weatherApp.showForecast = forecastData => {
     currentTimeElement.textContent = `${currentDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`;
 
     const dailyTemperatureElement = document.querySelector('#current-daily-temperature');
-    const dailyHighTemperature = Math.round(forecastData.daily[0].temp.max - 273.15);
-    const dailyLowTemperature = Math.round(forecastData.daily[0].temp.min - 273.15);
+    const dailyHighTemperature = Math.round(forecastData.daily[0].temp.max);
+    const dailyLowTemperature = Math.round(forecastData.daily[0].temp.min);
 
-    dailyTemperatureElement.textContent = `${dailyHighTemperature}°C / ${dailyLowTemperature}°C`;
+    dailyTemperatureElement.textContent = `${dailyHighTemperature}° / ${dailyLowTemperature}°`;
 
     const daysOfForecast = document.querySelectorAll('.forecast');
 
     for (let i = 0; i < daysOfForecast.length; i++) {
         const forecastHeadings = document.querySelectorAll('h4');
-        forecastHeadings[i].textContent = new Date(forecastData.daily[i + 1].dt * 1000).toLocaleString('en-US', { weekday: 'short', month: 'short', day: '2-digit' });
+        forecastHeadings[i].textContent = new Date(forecastData.daily[i + 1].dt * 1000).toLocaleString('en-US', {
+            weekday: 'short',
+            month: 'short',
+            day: '2-digit'
+        });
         const forecastTemperatureElement = document.querySelectorAll('.forecast-daily-temperature');
-        const forecastHighTemperature = Math.round(forecastData.daily[i+1].temp.max - 273.15);
-        const forecastLowTemperature = Math.round(forecastData.daily[i+1].temp.min - 273.15);
-        forecastTemperatureElement[i].textContent= `${forecastHighTemperature}°C / ${forecastLowTemperature}°C`;
+        const forecastHighTemperature = Math.round(forecastData.daily[i + 1].temp.max);
+        const forecastLowTemperature = Math.round(forecastData.daily[i + 1].temp.min);
+        forecastTemperatureElement[i].textContent = `${forecastHighTemperature}° / ${forecastLowTemperature}°`;
         const forecastIcon = document.createElement('i');
-        forecastIcon.classList.add('wi',`wi-owm-${ forecastData.daily[i + 1].weather[0].id}`);
-        forecastHeadings[i].insertAdjacentElement('afterend' , forecastIcon);
+        forecastIcon.classList.add('wi', `wi-owm-${ forecastData.daily[i + 1].weather[0].id}`);
+        forecastHeadings[i].insertAdjacentElement('afterend', forecastIcon);
     }
 }
 
 weatherApp.init = () => {
     weatherApp.getVariables();
+    weatherApp.checkUnit();
     weatherApp.getLocation();
 };
 
